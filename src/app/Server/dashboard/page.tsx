16253c850aa303583a07b289319d6cc585cedb1b
THOTAKURA-PRASANTH <211fa04025@gmail.com>
2025-08-18 14:58:41 +0530

    hello

--- src/app/Server/dashboard/page.tsx
+++ src/app/Server/dashboard/page.tsx
@@ -4,8 +4,5 @@ import DashboardClient from './dashboardClient'; // The UI part
 export default async function DashboardPage() {
   const supabase = await createClient();
   const { data: { user } } = await supabase.auth.getUser();
-  if (!user) {
-    return <div>User not found.</div>;
-  }
   return <DashboardClient user={user} />;
 }
